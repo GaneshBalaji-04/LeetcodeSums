61c207a3fdef6325671710b2354b654af7f8ddd2
GaneshBalaji-04 <balajibarani2004@protonmail.com>
2025-01-21 06:33:40 +0530

    Combination Sum II

--- Backtracking/LC#40.cpp
+++ Backtracking/LC#40.cpp
@@ -12,6 +12,7 @@ class Solution {
         for (int i = start; i < nums.size(); i++) {
             if (i > start && nums[i] == nums[i - 1])
                 continue;
+            /* This if condition is very much important to avoid duplicate presence of the elements. There are test cases such as such as [1,2,2,2,5] by which [1,2,2] can be added but another 2 is not needed. In the test case [1,1,....1(100 times)], first 30 satisfies the target. Next ones are not needed and so, no need to be checked.*/
             curr.push_back(nums[i]);
             backtrack(i + 1, result, curr, nums, target - nums[i]);
             curr.pop_back();
